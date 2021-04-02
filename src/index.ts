75952feef7a51b639e3d2cc1495950c2c6486a0f
jackluson <18219112108@163.com>
2021-04-03 01:12:32 +0800

    feat: :hammer: init commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/*
+ * Desc: a entry script
+ * File: /src/index.ts
+ * Project: node-typescript-template
+ * File Created: Saturday, 3rd April 2021 12:31:28 am
+ * Author: luxuemin2108@gmail.com
+ * -----
+ */
+
+setInterval(function () {
+  console.log('timer that keeps nodejs processing running');
+}, 1000 * 60 * 60);
